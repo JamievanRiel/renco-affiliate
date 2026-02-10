3113484e47fd1272e86ce3762f9fae17100e7456
Jamie <102923504+JamievanRiel@users.noreply.github.com>
2026-02-10 10:07:21 +0100

    Change base path in Vite config for GitHub Pages
    Update base path for GitHub Pages deployment.

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Door './' te gebruiken, werken de paden overal, 
-  // ongeacht of je in een submap staat op GitHub.
-  base: './', 
-});
+  // Dit zorgt ervoor dat alle linkjes naar CSS en JS kloppen op GitHub Pages
+  base: '/renco-affiliate/', 
+})
